2e69023690b6363defc7c921dd381f6bcb1b6a7a
dio222222 <dennisnklp@gmail.com>
2022-10-21 16:06:12 +0300

    Constructed Data Collector parameters array
    To be saved in the database

--- app/WeatherAPI/DataCollector.php
+++ app/WeatherAPI/DataCollector.php
@@ -24,7 +24,80 @@
 
             }
 
-            return $response;
+            // Convert Temperature Parameter
+            [$celcius, $fahrenheit] = $this->convert_kelvin($response['main']['temp']);
+
+            // Pass Converted Data as Extra Parameters
+            $response['main']['temp'] = array(
+
+                'kelvin' => $response['main']['temp'],
+                'celcius' => $celcius,
+                'fahrenheit' => $fahrenheit,
+
+            );
+
+            // Convert Feels Like Parameter
+            [$celcius, $fahrenheit] = $this->convert_kelvin($response['main']['feels_like']);
+
+            // Pass Converted Data as Extra Parameters
+            $response['main']['feels_like'] = array(
+
+                'kelvin' => $response['main']['feels_like'],
+                'celcius' => $celcius,
+                'fahrenheit' => $fahrenheit,
+
+            );
+
+            // Convert Minimum Temperature Parameter
+            [$celcius, $fahrenheit] = $this->convert_kelvin($response['main']['temp_min']);
+
+            // Pass Converted Data as Extra Parameters
+            $response['main']['temp_min'] = array(
+
+                'kelvin' => $response['main']['temp_min'],
+                'celcius' => $celcius,
+                'fahrenheit' => $fahrenheit,
+
+            );
+            
+            // Convert Maximum Temperature Parameter
+            [$celcius, $fahrenheit] = $this->convert_kelvin($response['main']['temp_max']);
+
+            // Pass Converted Data as Extra Parameters
+            $response['main']['temp_max'] = array(
+
+                'kelvin' => $response['main']['temp_max'],
+                'celcius' => $celcius,
+                'fahrenheit' => $fahrenheit,
+
+            );
+
+            // Construct Results Array to Be saved in the Database
+            $results = array(
+
+                'date' => $this->convert_unix_to_date($response['dt']),
+                'temp_kelvin' => $response['main']['temp']['kelvin'],
+                'temp_celcius' => $response['main']['temp']['celcius'],
+                'temp_fahrenheit' => $response['main']['temp']['fahrenheit'],
+                'feels_like_kelvin' => $response['main']['feels_like']['kelvin'],
+                'feels_like_celcius' => $response['main']['feels_like']['celcius'],
+                'feels_like_fahrenheit' => $response['main']['feels_like']['fahrenheit'],
+                'temp_min_kelvin' => $response['main']['temp_min']['kelvin'],
+                'temp_min_celcius' => $response['main']['temp_min']['celcius'],
+                'temp_min_fahrenheit' => $response['main']['temp_min']['fahrenheit'],
+                'temp_max_kelvin' => $response['main']['temp_max']['kelvin'],
+                'temp_max_celcius' => $response['main']['temp_max']['celcius'],
+                'temp_max_fahrenheit' => $response['main']['temp_max']['fahrenheit'],
+                'pressure' => $response['main']['pressure'],
+                'humidity' => $response['main']['humidity'],
+                'visibility' => $response['visibility'],
+                'wind_speed' => $response['wind']['speed'],
+                'wind_deg' => $response['wind']['deg'],
+                'clouds' => $response['clouds']['all'],
+
+            );
+
+            return $results;
 
         }
 

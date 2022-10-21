80a761e98d75a3d4e28bde6882d39699cbf4561d
dio222222 <dennisnklp@gmail.com>
2022-10-22 00:55:40 +0300

    Added Authorization to Endpoints

--- api/forecast.php
+++ api/forecast.php
@@ -1,8 +1,12 @@
 <?php
 
     require '../boot/boot.php';
+    require '../vendor/autoload.php';
+    require '../scripts/helper_functions.php';
 
     use WeatherAPI\Forecast;
+    use Firebase\JWT\JWT;
+    use Firebase\JWT\Key;
 
     // Response Headers
     header("Access-Control-Allow-Origin: *");
@@ -10,6 +14,8 @@
     header("Access-Control-Allow-Methods: POST");
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+    $key = get_secret_key();
 
     // Only Accept GET Request Methon
     if (strtolower($_SERVER['REQUEST_METHOD'] != 'GET')) {
@@ -24,6 +30,52 @@
         exit();
 
     }
+
+    // Get Request Headers
+    $headers = getallheaders();
+
+    // Check if Bearer Token is passed in Authorization Header 
+    if (!isset($headers['Authorization'])) {
+
+        $response['message'] = 'Invalid Token';
+
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+        http_response_code(401);
+
+        $response = json_encode($response);
+
+        print_r($response);
+
+        exit();
+
+    }
+
+    // Get Token
+    $jwt = str_replace('Bearer ', '', $headers['Authorization']);
+
+    // Validate Token
+    try {
+
+        $jwt_decoded = JWT::decode($jwt, new Key($key, 'HS512'));
+
+    }
+
+    catch (Exception $e) {
+
+        $response['message'] = 'Invalid Token';
+
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+        http_response_code(401);
+
+        $response = json_encode($response);
+
+        print_r($response);
+
+        exit();
+
+    }
+
+    // Authenticated
 
     if (!isset($_GET['city'])) {
 
